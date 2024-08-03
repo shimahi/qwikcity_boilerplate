5ca5053bf70c54079611d7cff52fd9fd0e94d713
shimahi <shimahi@reah.jp>
2024-08-04 03:03:18 +0900

    Menu,MenuContent削除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,11 @@
 import { UserDomain } from '@/domains/user'
 
 import type { User } from '@/schemas'
-import type { AuthUser } from '@/services/kv'
 import { css } from '@/styled-system/css'
 import { container } from '@/styled-system/patterns'
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import { routeLoader$ } from '@builder.io/qwik-city'
-import { Modal } from '@qwik-ui/headless'
-import { LuMenu, LuX } from '@qwikest/icons/lucide'
 
 export const useLoader = routeLoader$(async (requestEvent) => {
   const userDomain = new UserDomain(requestEvent)
@@ -172,81 +169,6 @@ export const Contents = component$(({ users }: { users: User[] }) => {
     </div>
   )
 })
-
-export const Menu = component$(
-  ({ currentUser }: { currentUser: AuthUser | null }) => {
-    return (
-      <>
-        <Modal.Root
-          class={css({
-            md: {
-              display: 'none',
-            },
-          })}
-        >
-          <Modal.Trigger
-            class={css({
-              position: 'fixed',
-              top: 5,
-              right: 5,
-              zIndex: 3,
-            })}
-          >
-            <LuMenu color="white" font-size={32} />
-          </Modal.Trigger>
-          <Modal.Panel>
-            <div
-              class={css({
-                position: 'fixed',
-                top: 0,
-                right: 0,
-                height: '100%',
-                width: '100%',
-                backgroundColor: 'gray.100',
-              })}
-            >
-              <Modal.Close
-                class={css({
-                  position: 'fixed',
-                  top: 5,
-                  right: 5,
-                  zIndex: 3,
-                })}
-              >
-                <LuX font-size={32} />
-              </Modal.Close>
-              {/* <MenuContent currentUser={currentUser} /> */}
-            </div>
-          </Modal.Panel>
-        </Modal.Root>
-        <div
-          class={css({
-            paddingTop: '64px',
-            backgroundColor: 'gray.100',
-            position: 'fixed',
-            top: 0,
-            right: 0,
-            height: '100%',
-            zIndex: 2,
-            width: '300px',
-            display: 'none',
-            md: {
-              display: 'block',
-            },
-          })}
-        >
-          {/* <MenuContent currentUser={currentUser} /> */}
-        </div>
-      </>
-    )
-  },
-)
-
-export const MenuContent = component$(
-  ({ currentUser }: { currentUser: AuthUser | null }) => {
-    return currentUser ? <></> : <></>
-  },
-)
 
 export const head: DocumentHead = {
   title: 'Qwik Summer',
